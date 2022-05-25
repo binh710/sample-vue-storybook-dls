27e89b2c5047bfef8866f4e7ecb13fc9d97b7211
Binh <thanhbinh710@gmail.com>
2022-05-25 09:04:18 +0800

    Add code for color mdx

--- src/stories/Foundation/Colour/colourBlock.vue
+++ src/stories/Foundation/Colour/colourBlock.vue
@@ -1,13 +1,40 @@
 <template>
-  Sth here
+  <div class="color-block" :key="id">
+    <div
+      :style="style"
+      class="color-block--box"
+    ></div>
+    <div class="color-block--text">
+      <p>{{ name }}</p>
+      <p>{{ color }}</p>
+    </div>
+  </div>
 </template>
 
 <script>
 import './colourBlock.scss';
+import { reactive, computed } from 'vue';
 
 export default {
-  name: 'my-button',
-  props: {}
+  name: 'colour-block',
+  props: {
+    id: String,
+    color: String,
+    border: String,
+    name: String,
+  },
+
+  setup(props, { emit }) {
+    props = reactive(props);
+    
+    return {
+      style: computed(() => ({
+        backgroundColor: props.color,
+        borderColor: props.border ? props.border : 'transparent',
+      })),
+    }
+  },
+  
 }
 
 </script>
